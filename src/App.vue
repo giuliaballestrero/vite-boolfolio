ff32f872999a452abd70bcd6f84a41f54e809af2
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-02 17:54:41 +0100

    Show Projects list in AppMain

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppMain from './components/AppMain.vue';
 
 export default {
+
   components: {
     AppMain,
   }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,24 +1,80 @@
 <script>
-//to do -- import card component ;
+import axios from 'axios';
+//to do -- import card component 
 
 export default {
 
   name: 'AppMain',
-  
-  components: {
 
+  components: {
+//to do -- import card component
   },
 
   data () {
+    return {
+      projects: [],
+      loading: false,
+      urlAddress: 'http://127.0.0.1:8000/api/projects',
+    }
+  },
 
-  }
+  methods: {
+    getprojects () {
+      axios.get(this.urlAddress, {
+        params: {
+          //
+        }
+      })
+      .then((response) => {
+        console.log(response.data.results.data);
+        this.projects= response.data.results.data;
+    })   
+    }
+  },
+  created() {
+    this.getprojects();
+  },
+    
 }
+
 
 </script>
 
 <template>
 
-  <h1 class="text-warning">Hello World</h1>
+  <section>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-12">
+            <h1 class="pb-5">Projects list:</h1>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-3" v-for="project in projects">
+            <div class="card" style="width: 18rem;">
+              <span class="card-header">{{ project.slug }}</span>
+              <span>{{ project.type.name }}</span>
+              <img :src="project.thumb" class="card-img-top" :alt="project.title">
+              <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">{{ project.description }}</p>
+                <p class="card-text">{{ project.creation_date }}</p>
+                <p>
+                  <span class="me-2" v-for="technology in project.technologies">
+                    {{ technology.name }}
+                  </span>
+                </p>
+                <a href="#" class="btn btn-primary">Show more</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+  </section>
 
 </template>
 
