c52010d0038a6f11e498ce8452c85a6387793885
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-02 16:53:22 +0100

    Add new Vue project and install bootstrap + scss

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppMain,
+  }
+}
+
+</script>
+
+<template>
+
+  <AppMain/>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+//to do -- import card component ;
+
+export default {
+
+  name: 'AppMain',
+  
+  components: {
+
+  },
+
+  data () {
+
+  }
+}
+
+</script>
+
+<template>
+
+  <h1 class="text-warning">Hello World</h1>
+
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
