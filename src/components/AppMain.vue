e11f03d442585c2117867413b83e3073ff739fdd
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-02 18:52:11 +0100

    Add new component for Projects cards and add store

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,24 @@
 <script>
-import axios from 'axios';
-//to do -- import card component 
+  //import Axios
+  import axios from 'axios';
+  import { store } from '../store.js';
+  //console.log(store); 
+
+  //import project component
+  import ProjectComponent from './ProjectComponent.vue'; 
 
 export default {
 
   name: 'AppMain',
 
   components: {
-//to do -- import card component
+    ProjectComponent
   },
 
   data () {
     return {
+      store,
+
       projects: [],
       loading: false,
       urlAddress: 'http://127.0.0.1:8000/api/projects',
@@ -19,6 +26,7 @@ export default {
   },
 
   methods: {
+    //create axios call to show laravel projects
     getprojects () {
       axios.get(this.urlAddress, {
         params: {
@@ -26,8 +34,8 @@ export default {
         }
       })
       .then((response) => {
-        console.log(response.data.results.data);
-        this.projects= response.data.results.data;
+        //console.log(response.data.results.data);
+        this.store.projectList= response.data.results.data;
     })   
     }
   },
@@ -45,32 +53,14 @@ export default {
   <section>
       <div class="container">
 
-        <div class="row">
-          <div class="col-12">
-            <h1 class="pb-5">Projects list:</h1>
-          </div>
-        </div>
+        <h1 class="pb-5">Projects list:</h1>
 
-        <div class="row">
-          <div class="col-3" v-for="project in projects">
-            <div class="card" style="width: 18rem;">
-              <span class="card-header">{{ project.slug }}</span>
-              <span>{{ project.type.name }}</span>
-              <img :src="project.thumb" class="card-img-top" :alt="project.title">
-              <div class="card-body">
-                <h5 class="card-title">{{ project.title }}</h5>
-                <p class="card-text">{{ project.description }}</p>
-                <p class="card-text">{{ project.creation_date }}</p>
-                <p>
-                  <span class="me-2" v-for="technology in project.technologies">
-                    {{ technology.name }}
-                  </span>
-                </p>
-                <a href="#" class="btn btn-primary">Show more</a>
-              </div>
-
-            </div>
-          </div>
+        <div class="card-wrapper">
+          <!--Qui andranno le card-->
+        
+          <ProjectComponent 
+            v-for="projectElement in store.projectList" :project="projectElement"
+          />
         </div>
 
       </div>
---
+++ src/components/ProjectComponent.vue
@@ -0,0 +1,52 @@
+<script>
+
+export default {
+
+    name: 'ProjectComponent',
+  
+    props: {
+        project: Object,
+    },
+
+    data () {
+    return {
+
+    }
+    }
+}
+</script>
+
+<template>
+        <div class="card text-center mb-3">
+            <div class="card-body p-3 m-3">
+                <h2 class="card-title fw-bold p-3">
+                    {{ project.title }}
+                </h2>
+
+                <img :src="project.thumb" class="img-thumb" :alt="project.title">
+
+                <p class="card-text pt-4 mb-4">
+                    {{ project.description }}
+                </p>
+                <p class="fw-bold">
+                    Type: 
+                    <a href="" class="btn btn-disabled rounded-pill fw-bold ms-2">
+                        {{project.type.name}}
+                    </a>
+                </p>
+
+                <span class="fw-bold me-2">Made with (&hearts;): </span>
+                    <a class="btn btn-disabled rounded-pill fw-bold me-2" v-for="technology in project.technologies">
+                        #{{technology.name}}
+                    </a> 
+            </div>
+            
+            <div class="card-footer text-muted">
+                Created on {{ project.creation_date }} - Proj. id: {{ project.slug }}
+            </div>
+        </div>   
+</template>
+
+<style lang="scss" scoped>
+
+</style>
